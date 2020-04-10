ce1563eae3e537701778e6c718293d92c86251d5
Manuel Ojeda <manuelojedasistemas@gmail.com>
2020-04-10 14:48:54 -0600

    Migrations added

---
+++ database/migrations/2020_04_10_204107_create_jobs_table.php
@@ -0,0 +1,36 @@
+<?php
+// phpcs:disable
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->json('content');
+            $table->string('thumbnail')->nullable();
+            $table->json('images')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
